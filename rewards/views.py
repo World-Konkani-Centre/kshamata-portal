73f6b9ee9fd9df3195d9e9d29ba59a909e25f450
swasthikcnayak <swasthik0304@gmail.com>
2021-05-12 12:15:59 +0530

    basic directory structure

---
+++ rewards/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def give_award(request):
+    pass
+
+
+def top_list(request):
+    pass
+
+
+def leaderboard(request):
+    pass
