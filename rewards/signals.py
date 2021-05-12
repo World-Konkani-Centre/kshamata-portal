02bec7536e7030238609d4c8c178f585ca59d923
swasthikcnayak <swasthik0304@gmail.com>
2021-05-12 13:34:51 +0530

    sorting out the signals

--- rewards/signals.py
+++ rewards/signals.py
@@ -11,7 +11,7 @@ def update_team_points(sender, instance, created, **kwargs):
     if created:
         if instance.user:
             team = instance.user.profile
-            team.points = team.points + instance.points
+            team.team_points = team.team_points + instance.points
             team.save()
 
 
