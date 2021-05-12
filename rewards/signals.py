9f5cba96ab472be6fed5b7d894624fd18a56e646
swasthikcnayak <swasthik0304@gmail.com>
2021-05-12 13:31:42 +0530

    basic directory structure

--- rewards/signals.py
+++ rewards/signals.py
@@ -3,11 +3,15 @@ from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 
-# from .models import Points
-#
-#
-# @receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         if instance.team:
-#
+from .models import Points
+
+
+@receiver(post_save, sender=Points)
+def update_team_points(sender, instance, created, **kwargs):
+    if created:
+        if instance.user:
+            team = instance.user.profile
+            team.points = team.points + instance.points
+            team.save()
+
+
