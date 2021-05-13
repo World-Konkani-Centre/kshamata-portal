35ac58df79e17ab76503be96c4b230039a551cd3
Praneeth <18praneeth@gmail.com>
2021-05-13 13:19:01 +0530

    Added extra fields to Profile

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.db.models.functions import Lower
+from django.utils.translation import gettext_lazy as _
 from .models import Profile
 
 
@@ -19,6 +19,23 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class ProfieCreateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'gender', 'batch', 'date_of_birth','college','image']
+        labels = {
+            'name': _('Full Name'),
+            'gender': _('Gender'),
+            'batch': _('VKSSF Batch'),
+            'date_of_birth': _('Date of Birth'),
+            'image': _('Upload your Profile Picture')
+        }
+        help_texts = {
+            'name': _('Name will be displayed to other users'),
+        }
+
 
 
 class ProfileUpdateForm(forms.ModelForm):
--- users/models.py
+++ users/models.py
@@ -1,9 +1,14 @@
+from typing import Tuple
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db.models import Sum
 from PIL import Image
 
+GENDER = [
+    ('MALE', 'MALE'),
+    ('FEMALE', 'FEMALE'),
+]
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
@@ -14,6 +19,8 @@ class Team(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100,null=True,blank=True)
+    date_of_birth =  models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=50, choices=GENDER, null=True, blank=True)
     batch = models.IntegerField(null=True,blank=True)
     team = models.ForeignKey(Team,on_delete=models.CASCADE,null=True,blank=True)
     college = models.CharField(max_length=100,null=True,blank=True)
