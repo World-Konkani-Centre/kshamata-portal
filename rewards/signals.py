461423a55bfefd97f1ad9a743416038c3b5ee45f
Praneeth <18praneeth@gmail.com>
2021-05-28 21:23:02 +0530

    Updated signals

--- rewards/signals.py
+++ rewards/signals.py
@@ -22,3 +22,18 @@ def update_team_points(sender, instance, created, **kwargs):
             team.save()
 
 
+@receiver(pre_delete, sender=Points)
+def reduce_points(sender, instance, using, **kwargs):
+    if instance.user:
+        team = instance.user.profile.team
+        if team:
+            team.team_points = team.team_points - instance.points
+            team.save()
+            instance.user.profile.points = instance.user.profile.points - instance.points
+            instance.user.profile.save()
+    elif instance.team:
+        team = instance.team
+        team.team_points = team.team_points - instance.points
+        team.save()
+
+
