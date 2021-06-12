f0c82183f32b962c3f0211b2b9dcb1796956fdbf
Praneeth <18praneeth@gmail.com>
2021-06-12 12:32:02 +0530

    Updated minor space issue

--- rewards/urls.py
+++ rewards/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('award/',give_award , name='give_award'),
     path('award-list/<str:camp>', award_list, name='award-list'),
-    path('leaderboard/<str:camp>',leaderboard,name="leaderboard"),
+    path('leaderboard/<str:camp>', leaderboard,name="leaderboard"),
 ]
